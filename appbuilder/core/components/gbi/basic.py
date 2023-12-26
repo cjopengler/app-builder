264a1412d5da0a2b34849242f55220fad80c47c0
cjopengler <cjopengler@163.com>
2023-12-26 10:12:06 +0800

    优化 注释

--- appbuilder/core/components/gbi/basic.py
+++ appbuilder/core/components/gbi/basic.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python 3
 # -*- coding: utf-8 -*-
-
+# Copyright (c) 2023 Baidu, Inc. All Rights Reserved.
 #
-# Copyright (c) 2023 PanXu, Inc. All Rights Reserved
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-"""
-brief
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-Authors: PanXu
-Date:    2023/12/23 20:07:00
+r"""GBI nl2sql component.
 """
 
 from typing import Dict, List
